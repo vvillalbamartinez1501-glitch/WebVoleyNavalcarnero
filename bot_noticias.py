51944de2f0fd56f9eca32ac9eb5f2568cb582280
vvillalbamartinez1501-glitch <vvillalbamartinez1501@gmail.com>
2026-09-14 18:59:37 +0200

    Limit image URLs to the first child post in news processing

--- bot_noticias.py
+++ bot_noticias.py
@@ -242,7 +242,7 @@ def main():
             
             urls_imagenes = []
             if post.get('childPosts'):
-                urls_imagenes = [child.get('displayUrl') for child in post.get('childPosts') if child.get('displayUrl')]
+                urls_imagenes = [child.get('displayUrl') for child in post.get('childPosts') if child.get('displayUrl')][:1]
             elif post.get('displayUrl'):
                 urls_imagenes = [post.get('displayUrl')]
             
